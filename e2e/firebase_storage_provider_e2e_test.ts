2d9d62f3e113f3837934b5f4cd3abe5980cf924f
João Santos <josan5368@gmail.com>
2023-12-12 10:56:15 +0100

    chore: server to test firebase storage provider.

---
+++ e2e/firebase_storage_provider_e2e_test.ts
@@ -0,0 +1,27 @@
+import { startServer } from "../mod.ts";
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA35H9gdAgo85RtgJ89ISAAZfoalE0iVMQ",
+//     authDomain: "pessoal-407411.firebaseapp.com",
+//     projectId: "pessoal-407411",
+//     storageBucket: "pessoal-407411.appspot.com",
+//     messagingSenderId: "1019035304478",
+//     appId: "1:1019035304478:web:5d758ce1c56cdd3575640f",
+//     measurementId: "G-30V9CJW3WC"
+// };
+
+startServer({tenants: [
+    {
+        name: "firebase",
+        jwkPath: "./jwk.json",
+        storage: [
+            "firebase_storage/firebase_storage_provider.ts",
+            "gs://pessoal-407411.appspot.com"
+        ],
+        repository: [
+            "flat_file/flat_file_node_repository.ts",
+            "/tmp/antbox/repository",
+        ]
+    }
+]})
